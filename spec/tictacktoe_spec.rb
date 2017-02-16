3e684744a0abba8e702b5fbe5d4d4b2a45272248
Alwin Solanky <alwinsol@yahoo.co.uk>
2017-02-16 12:04:35 +0000

    First failing test

--- spec/tictacktoe_spec.rb
+++ spec/tictacktoe_spec.rb
@@ -1,2 +1,14 @@
 require 'spec_helper.rb'
 require './lib/tictacktoe.rb'
+
+describe Position do
+  context '#new' do
+    it 'should initialize a new board' do
+      position = Position.new
+      position.board.should == %w(---
+                                  ---
+                                  ---)
+      position.turn.should == "x"
+    end
+  end
+end
